15e40fe89e30025273dba5cc567143e8c11382fd
Yujin Kwon <rnjsdb72@gmail.com>
2022-05-19 20:35:52 +0900

    delete function 'MakeDataset' and add auxiliary information in class 'CustomDatset'

--- data_utils.py
+++ data_utils.py
@@ -10,6 +10,10 @@ import torch.utils.data as data
 def load_all(data_path: str,
              test_size: float = 0.3):
     """ We load all the three file here to save time in each epoch. """
+    """
+    data_path: 데이터 경로
+    test_size: test data 비율
+    """
     train_data = pd.read_csv(
         data_path, dtype={0: np.int32, 1: np.int32})
 
@@ -22,7 +26,8 @@ def load_all(data_path: str,
     train_mat = sp.dok_matrix((user_num, item_num), dtype=np.float32)
     for x in train_data:
         train_mat[x[0], x[1]] = 1.0
-
+    
+    # 데이터 분할
     train_data, test_data = train_test_split(train_data, test_size=test_size, shuffle=True, random_state=0)
 
     return train_data, test_data, user_num, item_num, train_mat
@@ -31,6 +36,11 @@ def load_aux(data_path: str,
             aux_col: str,
              test_size: float = 0.3):
     """ We load all the three file here to save time in each epoch. """
+    """
+    data_path: 데이터 경로
+    aux_col: auxiliary 정보가 있는 column명
+    test_size: test data 비율
+    """
     train_data = pd.read_csv(
         data_path, dtype={0: np.int32, 1: np.int32})
 
@@ -39,31 +49,65 @@ def load_aux(data_path: str,
 
     train_data = train_data.values.tolist()
 
-    # load ratings as a dok matrix
+    # load auxiliary information as a dok matrix
     mat = sp.dok_matrix((user_num, item_num), dtype=np.float32)
     for x in train_data:
         mat[x[0], x[1]] = 1.0
-        
-    train_data, test_data = train_test_split(train_data, test_size=test_size, shuffle=True, random_state=0)
     
-    return train_data, test_data, mat
+    return mat
 
 class CustomDataset(data.Dataset):
-    def __init__(self, features, num_item,
-                 train_mat=None, num_ng=0, is_training=None):
+    def __init__(self, data_path_main : str,
+                 data_path_aux_user = None, aux_col_user = None,
+                 data_path_aux_item = None, aux_col_item = None,
+                 test_size : float = 0.3, num_ng=0, is_training=None):
         super(CustomDataset, self).__init__()
         """ Note that the labels are only useful when training, we thus 
             add them in the ng_sample() function.
         """
+        """
+        data_path_main: main data(rating matrix) 데이터 경로
+        data_path_aux_user: user auxiliary information 데이터 경로(들) -> 리스트 형태
+        aux_col_user: user axiliary information 정보가 있는 column명(들) -> 리스트 형태
+        data_path_aux_item: item auxiliary information 데이터 경로(들) -> 리스트 형태
+        aux_col_item: item axiliary information 정보가 있는 column명(들) -> 리스트 형태
+        test_size: test data 비율
+        num_ng: negative sampling 비율 (vs positive sample)
+        is_training: training 여부
+        """
+
+        # loading main data
+        train_data, test_data, user_num, item_num, train_mat = load_all(data_path_main, test_size)
+        
+        # loading user auxiliary information data
+        try:
+            for i in range(len(data_path_aux_user)):
+                self.globals()[f'mat_{aux_col_user[i]}'] = (data_path_aux_user[i], aux_col_user[i], test_size)
+        except:
+            pass
+        
+        # loading item auxiliary information data
+        try:
+            for i in range(len(data_path_aux_item)):
+                self.globals()[f'mat_{aux_col_item[i]}'] = (data_path_aux_item[i], aux_col_item[i], test_size)
+        except:
+            pass
+        
+        # 학습 여부에 따라 features 변수에 알맞는 데이터 할당
+        if is_training == True:
+            features = train_data
+        elif is_training == False:
+            features = test_data
         
         self.features_ps = features
-        self.num_item = num_item
+        self.num_item = item_num
         self.train_mat = train_mat
         self.num_ng = num_ng
         self.is_training = is_training
         self.labels = [0 for _ in range(len(features))]
 
     def ng_sample(self):
+        """negative sampling"""
         assert self.is_training, 'no need to sampling when testing'
 
         self.features_ng = []
@@ -82,6 +126,7 @@ class CustomDataset(data.Dataset):
         self.labels_fill = labels_ps + labels_ng
 
     def __len__(self):
+        """length of data"""
         return (self.num_ng + 1) * len(self.labels)
 
     def __getitem__(self, idx):
@@ -90,106 +135,17 @@ class CustomDataset(data.Dataset):
         labels = self.labels_fill if self.is_training \
                     else self.labels
 
-        user = features[idx][0]
-        item = features[idx][1]
-        label = labels[idx]
-        return user, item ,label
-    
-def Make_DataSet(data_path_main : str,
-                 test_size : float = 0.3,
-                 num_ng: int = 4,
-                 batch_size: int = 32,
-                 data_path_user_aux = None,
-                 cols_user_aux = None,
-                 data_path_item_aux = None,
-                 cols_item_aux = None):
-    # main data 생성
-    train_data_main, test_data_main, user_num_main, item_num_main, train_mat_main\
-        = load_all(data_path_main, test_size)
-    
-    train_dataset_main = CustomDataset(train_data_main, item_num_main, train_mat_main, num_ng, True)
-    test_dataset_main = CustomDataset(test_data_main, item_num_main, train_mat_main, 0, False)
-    train_loader_main = data.DataLoader(train_dataset_main, batch_size=batch_size, shuffle=True)
-    test_loader_main = data.DataLoader(test_dataset_main, batch_size=batch_size, shuffle=False) # 여기 batch_size 어떻게 해야 할까요?
-    
-    # user auxiliary data 생성
-    train_dataset_user_auxes = []
-    test_dataset_user_auxes = []
-    train_loader_user_auxes = []
-    test_loader_user_auxes = []
-    try:
-        for i in range(len(data_path_user_aux)):
-            # 데이터, 행렬 불러오기
-            globals()[f'train_data_{cols_user_aux[i]}'], globals()[f'test_data_{cols_user_aux[i]}'],\
-            globals()[f'mat_{cols_user_aux[i]}']\
-                = load_aux(data_path_user_aux[i], cols_user_aux[i], test_size)
-            # target 생성
-            globals()[f'train_targets_{cols_user_aux[i]}']\
-                = sp.vstack([globals()[f'mat_{cols_user_aux[i]}'][user] for user, aux in globals()[f'train_data_{cols_user_aux[i]}']])
-            globals()[f'test_targets_{cols_user_aux[i]}']\
-                = sp.vstack([globals()[f'mat_{cols_user_aux[i]}'][user] for user, aux in globals()[f'test_data_{cols_user_aux[i]}']])
-            mat_tr = globals()[f'train_targets_{cols_user_aux[i]}']
-            mat_te = globals()[f'test_targets_{cols_user_aux[i]}']
-            mat_tr = torch.sparse.LongTensor(torch.LongTensor([mat_tr.tocoo().row.tolist(), mat_tr.tocoo().col.tolist()]),
-                           torch.LongTensor(mat_tr.tocoo().data.astype(np.int32)))
-            mat_te = torch.sparse.LongTensor(torch.LongTensor([mat_te.tocoo().row.tolist(), mat_te.tocoo().col.tolist()]),
-                           torch.LongTensor(mat_te.tocoo().data.astype(np.int32)))
-            # dataset 생성
-            globals()[f'train_dataset_{cols_user_aux[i]}']\
-            = data.TensorDataset(torch.tensor(globals()[f'train_data_{cols_user_aux[i]}']), mat_tr)
-            globals()[f'test_dataset_{cols_user_aux[i]}']\
-            = data.TensorDataset(torch.tensor(globals()[f'test_data_{cols_user_aux[i]}']), mat_te)
-            # data loader 생성
-            globals()[f'train_loader_{cols_user_aux[i]}'] = data.DataLoader(globals()[f'train_dataset_{cols_user_aux[i]}'], batch_size=batch_size, shuffle=True)
-            globals()[f'test_loader_{cols_user_aux[i]}'] = data.DataLoader(globals()[f'test_dataset_{cols_user_aux[i]}'], batch_size=batch_size, shuffle=False) # 여기 batch_size 어떻게 해야 할까요?
-            train_dataset_user_auxes.append(globals()[f'train_dataset_{cols_user_aux[i]}'])
-            test_dataset_user_auxes.append(globals()[f'test_dataset_{cols_user_aux[i]}'])
-            train_loader_user_auxes.append(globals()[f'train_loader_{cols_user_aux[i]}'])
-            test_loader_user_auxes.append(globals()[f'test_loader_{cols_user_aux[i]}'])
-    except:
-        pass
-    
-    # item auxiliary data 생성
-    train_dataset_item_auxes = []
-    test_dataset_item_auxes = []
-    train_loader_item_auxes = []
-    test_loader_item_auxes = []
-    try:
-        for i in range(len(data_path_item_aux)):
-            # 데이터, 행렬 불러오기
-            globals()[f'train_data_{cols_item_aux[i]}'], globals()[f'test_data_{cols_item_aux[i]}'],\
-            globals()[f'mat_{cols_item_aux[i]}']\
-                = load_aux(data_path_item_aux[i], cols_item_aux[i], test_size)
-            # target 생성
-            globals()[f'train_targets_{cols_item_aux[i]}']\
-                = sp.vstack([globals()[f'mat_{cols_item_aux[i]}'][user] for user, aux in globals()[f'train_data_{cols_item_aux[i]}']])
-            globals()[f'test_targets_{cols_item_aux[i]}']\
-                = sp.vstack([globals()[f'mat_{cols_item_aux[i]}'][user] for user, aux in globals()[f'test_data_{cols_item_aux[i]}']])
-            mat_tr = globals()[f'train_targets_{cols_item_aux[i]}']
-            mat_te = globals()[f'test_targets_{cols_item_aux[i]}']
-            mat_tr = torch.sparse.LongTensor(torch.LongTensor([mat_tr.tocoo().row.tolist(), mat_tr.tocoo().col.tolist()]),
-                           torch.LongTensor(mat_tr.tocoo().data.astype(np.int32)))
-            mat_te = torch.sparse.LongTensor(torch.LongTensor([mat_te.tocoo().row.tolist(), mat_te.tocoo().col.tolist()]),
-                           torch.LongTensor(mat_te.tocoo().data.astype(np.int32)))
-            # dataset 생성
-            globals()[f'train_dataset_{cols_item_aux[i]}']\
-            = data.TensorDataset(torch.tensor(globals()[f'train_data_{cols_item_aux[i]}']), mat_tr)
-            globals()[f'test_dataset_{cols_item_aux[i]}']\
-            = data.TensorDataset(torch.tensor(globals()[f'test_data_{cols_item_aux[i]}']), mat_te)
-            # data loader 생성
-            globals()[f'train_loader_{cols_item_aux[i]}'] = data.DataLoader(globals()[f'train_dataset_{cols_item_aux[i]}'], batch_size=batch_size, shuffle=True)
-            globals()[f'test_loader_{cols_item_aux[i]}'] = data.DataLoader(globals()[f'test_dataset_{cols_item_aux[i]}'], batch_size=batch_size, shuffle=False) # 여기 batch_size 어떻게 해야 할까요?
-            train_dataset_item_auxes.append(globals()[f'train_dataset_{cols_item_aux[i]}'])
-            test_dataset_item_auxes.append(globals()[f'test_dataset_{cols_item_aux[i]}'])
-            train_loader_item_auxes.append(globals()[f'train_loader_{cols_item_aux[i]}'])
-            test_loader_item_auxes.append(globals()[f'test_loader_{cols_item_aux[i]}'])
-    except:
-        pass
-    
-    results = {'train_dataset_main':train_dataset_main, 'test_dataset_main':test_dataset_main,
-              'train_loader_main':train_loader_main, 'test_loader_main':test_loader_main,
-              'train_dataset_user_auxes':train_dataset_user_auxes, 'test_dataset_user_auxes':test_dataset_user_auxes,
-              'train_loader_user_auxes':train_loader_user_auxes, 'test_loader_user_auxes':test_loader_user_auxes,
-              'train_dataset_item_auxes':train_dataset_item_auxes, 'test_dataset_item_auxes':test_dataset_item_auxes,
-              'train_loader_item_auxes':train_loader_item_auxes, 'test_loader_item_auxes':test_loader_item_auxes}
-    return results
+        # user, item, label
+        user = torch.LongTensor(features[idx][0])
+        item = torch.LongTensor(features[idx][1])
+        label_main = torch.LongTensor(labels[idx])
+        
+        results = {'user_id':user, 'item_id':item, 'target_main':label_main}
+        
+        # auxiliary information
+        for i in range(len(data_path_aux_user)):
+            results.update( {f'target_user_aux_{aux_col_user[i]}' : torch.LongTensor(self.globals()[f'mat_{aux_col_user[i]}'][user])} )
+        for i in range(len(data_path_aux_item)):
+            results.update( {f'target_item_aux_{aux_col_item[i]}' : torch.LongTensor(self.globals()[f'mat_{aux_col_item[i]}'][item])} )
+        
+        return results
