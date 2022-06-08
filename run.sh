ac4f956392a889359b602681874e39ea5224b8b6
Yujin Kwon <rnjsdb72@gmail.com>
2022-06-08 18:36:59 +0900

    fix code - js to json

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-python train.py train_config1.js
-python train.py train_config2.js
-python train.py train_config3.js
-python train.py train_config4.js
+python train.py train_config1.json
+python train.py train_config2.json
+python train.py train_config3.json
+python train.py train_config4.json
